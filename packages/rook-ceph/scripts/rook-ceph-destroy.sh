9e1dd13068bfb7354dd002d407e69021a04c3cb8
Justin Law <justin.law@defenseunicorns.com>
2024-05-03 11:39:29 -0400

    destroy with optional var

--- packages/rook-ceph/scripts/rook-ceph-destroy.sh
+++ packages/rook-ceph/scripts/rook-ceph-destroy.sh
@@ -41,10 +41,15 @@ kubectl delete configmaps --all -n rook-ceph
 kubectl delete ns rook-ceph
 
 # Remove the remaining Rook-Ceph data on the host
-wipefs -a -f $DISK
-sgdisk --zap-all $DISK
-dd if=/dev/zero of="$DISK" bs=1M count=100 oflag=direct,dsync
-blkdiscard $DISK
-partprobe $DISK
+if [ -b "$DISK_NAME" ]; then
+    wipefs -a -f $DISK_NAME
+    sgdisk --zap-all $DISK_NAME
+    dd if=/dev/zero of="$DISK_NAME" bs=1M count=100 oflag=direct,dsync
+    blkdiscard $DISK_NAME
+    partprobe $DISK_NAME
+else
+    echo "The disk "$DISK_NAME" does not exist. Skipping wiping operations."
+fi
+
 
 rm -rf /var/lib/rook/

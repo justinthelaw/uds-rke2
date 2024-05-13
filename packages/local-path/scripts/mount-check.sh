2fd43a7c7daaf6054b9bb0cdd991edce9e7be0d5
Justin Law <justin.law@defenseunicorns.com>
2024-05-13 13:36:32 -0400

    add mount-check script fix pt.2

--- packages/local-path/scripts/mount-check.sh
+++ packages/local-path/scripts/mount-check.sh
@@ -3,7 +3,6 @@ set -e
 
 # Function to check mount point and print remaining space
 check_mount() {
-    local MOUNT_POINT="$0"
     if [ -d "$MOUNT_POINT" ]; then
         remaining_space=$(df -h "$MOUNT_POINT" | awk 'NR==2 {print $4}')
         echo "Mount Point: $MOUNT_POINT, Remaining Space: $remaining_space"
@@ -13,18 +12,17 @@ check_mount() {
 }
 
 # Check if mount points were provided as an argument
-if [ -z "$0" ]; then
-    echo "Usage: MOUNT_POINTS $1"
-    echo "Example: MOUNT_POINTS=\"/opt/ /dev/\" $1"
+if [ -z "$MOUNT_POINTS" ]; then
+    echo "Usage: MOUNT_POINTS=\"/opt/ /dev/\" $0"
     exit 1
 fi
 
 # Split the mount points string into an array
-IFS='/' read -ra MOUNT_POINTs_array <<< "$1"
+IFS=' ' read -ra MOUNT_POINTS_ARRAY <<<"$MOUNT_POINTS"
 
 # Loop through each mount point and check it
-for MOUNT_POINT in "${MOUNT_POINTs_array[@]}"; do
+for MOUNT_POINT in "${MOUNT_POINTS_ARRAY[@]}"; do
     if [ -n "$MOUNT_POINT" ]; then
-        check_mount "/$MOUNT_POINT"
+        check_mount "$MOUNT_POINT"
     fi
 done
